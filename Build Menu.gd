2fb17a9d43de780a40196c6b46efc3389e7447cf
Kjarrigan <github@kjarrigan.de>
2019-01-09 20:53:05 +0100

    Finish the node/rack placement

--- Build Menu.gd
+++ Build Menu.gd
@@ -14,27 +14,29 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
+var num_nodes = 0;
+var num_racks = 0
+const MAX_NODES = 21;
+const MAX_RACKS = 5
+
 func _on_Buy_Node_pressed():
 	
 	var racks = get_parent().find_node("Datacenter").get_children()
-	if racks.size() == 0:
-		prints("Leer")
+	if (num_racks * MAX_NODES) - num_nodes < 1:
+		prints("No empty rack slot left!")
 		return
-		
-	# TODO: look for next empty rack
-	var rack = racks[0]
-	
-	var scene = load("res://Node_A.tscn").instance()
-	rack.add_child(scene)
-	
-var num_racks = 0
-const MAX_RACKS = 5
+
+	var rack = racks[num_nodes / MAX_NODES]
+	var node = load("res://Node_A.tscn").instance()
+	node.position = Vector2(0, num_nodes % MAX_NODES * 18)
+	rack.add_child(node)
+	num_nodes += 1
 
 func _on_Buy_Rack_pressed():
 	if num_racks >= MAX_RACKS:
 		prints("Datacenter full")
 		return
 	num_racks += 1
-	var scene = load("res://Rack_A.tscn").instance()
-	scene.set_h_size_flags(SIZE_EXPAND_FILL)
-	get_parent().find_node("Datacenter").add_child(scene)
+	var rack = load("res://Rack_A.tscn").instance()
+	rack.set_h_size_flags(SIZE_EXPAND_FILL)
+	get_parent().find_node("Datacenter").add_child(rack)
